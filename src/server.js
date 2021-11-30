caa5db2b704dcff5ed6c274c3caf1953e14c9ed1
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-30 16:27:41 +0100

    structure + get

---
+++ src/server.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const server = express()
+
+const port = 3001
+server.listen(port, () => {})
---
+++ src/services/posts/post.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import uniqid from "uniqid"
+
+const postsRouter = express.Router()
+
+
+// take file path
+const postsPath = fileURLToPath(
+    import.meta.url)
+console.log("this is the current post path", postsPath)
+
+// take folder path
+const folderpostsPath = dirname(postsPath)
+console.log("this is the current folder path", folderpostsPath)
+
+//  access db file
+const postsDb = join(folderpostsPath, "postsDB.json")
+
+
+// GET METHOD
+// =============================
+
+userRouter.get("/", (req, res) => {
+    const postsCurrentFile = fs.readFileSync(postsDb)
+    console.log("file content:",
+        JSON.parse(postsCurrentFile))
+    const postsArray = JSON.parse(postsCurrentFile)
+    res.send(postsArray)
+
+})
+
+
+// GET METHOD specific ID
+// =============================
+userRouter.get("/:userid", (req, res) => {
+            const postsCurrentFile = fs.readFileSync(postsDb)
+
+
+
+
+            // POST METHOD
+            // =============================
+
+            // PUSH METHOD
+            // =============================
+
+
+            // DELETE METHOD
+            // =============================
+
+
+
+
+            export default postsRouter
