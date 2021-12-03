41017dd22f0b78c9558192390a443dfb167121fd
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-03 05:32:20 +0100

    refactoring functions

--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,41 +1,41 @@
 import fs from "fs-extra";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import multer from "multer";
+
 
 const { readJSON, writeJSON, writeFile } = fs;
 
-const postsPath = join(dirname(fileURLToPath(
+const dataPath = join(dirname(fileURLToPath(
     import.meta.url)), "../data");
-console.log("this is the path", postsPath);
+console.log("this is the path", dataPath);
+
 
 const usersPath = join(dirname(fileURLToPath(
     import.meta.url)), "../data");
 
 
-const postsPathJSON = join(postsPath, "postsDB.json");
+const dataPathJSON = join(dataPath, "postsDB.json");
+
+export const publicPath = join(process.cwd(), 'public');
+console.log("public path", publicPath);
+
 
 const usersPathJSON = join(usersPath, "usersDB.json");
 
-export const getPost = () => readJSON(postsPathJSON);
-export const writePost = (content) => writeJSON(postsPathJSON, content);
+const authorsAvatar = join(publicFolder, 'authors')
+
+const authorsCover = join(publicFolder, 'covers')
+
+export const getPost = () => readJSON(dataPathJSON);
+export const writePost = (content) => writeJSON(dataPathJSON, content);
 
 export const getUser = () => readJSON(usersPathJSON);
 export const writeUser = (content) => writeJSON(usersPathJSON, content);
 
 export const saveAvatar = (filename, contentAsABuffer) => {
-    join(writeFile(publicFolder, filename), contentAsABuffer);
+    join(writeFile(authorsAvatar, filename), contentAsABuffer);
 };
 
-export const parseImage = multer()
-export const upload = (req, res, next) => {
-    try {
-        console.log(req.file)
-
-        res.send("ok")
-
-    } catch (error) {
-        next(error)
-
-    }
-}
+export const saveCover = (filename, contentAsABuffer) => {
+    join(writeFile(authorsCover, filename), contentAsABuffer);
+};
