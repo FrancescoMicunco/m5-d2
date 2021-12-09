31b6e820854bfc666e18a06a380ef2f3cb0caeb9
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-09 17:49:01 +0100

    fixed auth errors

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,15 +1,24 @@
 import sgMail from "@sendgrid/mail"
 
-sgMail.setApiKey(process.env.SENDGRID_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export const sendRegistrationEmail = async recipientAddress => {
-    const msg = {
-        to: recipientAddress,
-        from: process.env.SENDER_EMAIL,
-        subject: "Let's try sending my first email server side!",
-        text: "Using Node.js ",
-        html: "<h3>A new world is waiting fors us... but, we need it?</h3>",
+    try {
+        const msg = {
+            to: recipientAddress,
+            from: process.env.SENDER_EMAIL,
+            subject: "Let's try sending my first email server side!",
+            text: "Using Node.js ",
+            html: "<h3>A new world is waiting fors us... but, we need it?</h3>",
+
+        }
+        const sendgrid = await sgMail.send(msg)
+
+        console.log(sendgrid)
+
+    } catch (error) {
+        console.log(error.response.body.errors)
     }
 
-    await sgMail.send(msg)
+
 }
